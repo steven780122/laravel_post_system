dcaf90b3d9b166696dd03450003b049637a3f540
steven780122 <steven780122@yahoo.com.tw>
2018-01-11 23:33:24 +0800

    update edit page

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -2,9 +2,11 @@
 
 @section("content")
     <div class="col-sm-8 blog-main">
-        <form action="/posts/62" method="POST">
-            <input type="hidden" name="_method" value="PUT">
-            <input type="hidden" name="_token" value="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy">
+        <form action="/posts/{{$post->id}}" method="POST">
+            {{--  <input type="hidden" name="_method" value="PUT">  --}}
+            {{method_field('PUT')}}
+            {{--  <input type="hidden" name="_token" value="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy">  --}}
+            {{csrf_field()}}
             <div class="form-group">
                 <label>標題</label>
                 <input name="title" type="text" class="form-control" placeholder="這裏是標題" value="{{$post->title}}">
